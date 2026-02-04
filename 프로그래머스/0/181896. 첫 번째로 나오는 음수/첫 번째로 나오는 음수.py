b7a9d17e5b4f95c3cca69aef707749f6648233c9
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-02-04 17:08:00 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,2 +1,6 @@
 def solution(num_list):
     return next((idx for idx, num in enumerate(num_list) if num < 0), -1)
+
+# next(), enumerate()
+# 인덱스와 값 둘 다 쓰면 enumerate, 인덱스만 쓰면 len
+# Python 커뮤니티에서는 enumerate를 더 pythonic하다고 본다.
