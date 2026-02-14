66111cb84a99e7a8512b87485a2028983afd882a
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-02-14 14:06:40 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,17 @@
+def solution(rsp):
+    answer = ''
+    for i in rsp:
+        if i == '2':
+            answer += '0'
+        elif i == '0':
+            answer += '5'
+        else:
+            answer += '2'
+    return answer
+
+"""
+가위 2 - 0
+바위 0 - 5
+보 5 - 2
+모두 이기는 경우?
+"""
