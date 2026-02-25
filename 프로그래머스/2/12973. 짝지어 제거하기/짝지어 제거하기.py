b8fd2c932f4a7887804126917d4cbf0279ce2b74
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-02-25 21:22:00 +0900

    [level 2] Title: 짝지어 제거하기, Time: 91.12 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,13 +1,10 @@
 def solution(s):
     stack = []
-    for i in s:
-        if not stack:  # 스택이 비어있으면
-            stack.append(i)
-            continue
-
-        if stack[-1] == i:  # 스택 맨 뒤 요소와 같으면
+    
+    for alpha in s:
+        if stack and stack[-1] == alpha:
             stack.pop()
-            continue
-            
-        stack.append(i)  # 위 조건이 모두 맞지 않으면
-    return 0 if stack else 1
+        else:
+            stack.append(alpha)
+    
+    return 1 if not stack else 0
