9f2c4ffc264025febf7b888eb7a6555e3a177dde
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-02-28 13:39:58 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,8 +1,2 @@
 def solution(nums):
-    answer = 0
-    
-    s = set(nums)   # 중복 제거
-    if len(s) > len(nums) // 2:
-        return len(nums) // 2
-    else:
-        return len(s)
+    return min(len(set(nums)), len(nums) // 2)
