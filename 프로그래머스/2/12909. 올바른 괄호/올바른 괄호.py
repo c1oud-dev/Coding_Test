366d3572859225c6531beac2aaa2eef6287e2dfb
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-03-05 13:28:19 +0900

    [level 2] Title: 올바른 괄호, Time: 6.91 ms, Memory: 9.51 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,9 +2,11 @@ def solution(s):
     stack = []
     
     for bracket in s:
-        if not stack or bracket == '(':
+        if bracket == '(':
             stack.append(bracket)
         else:
+            if not stack:
+                return False
             stack.pop()
-
-    return True if not stack else False
+            
+    return not stack
