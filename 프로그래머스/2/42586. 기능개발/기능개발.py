2255c85c73281c18376a7d3f93172bba246af0e0
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-03-05 12:48:48 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,16 +1,18 @@
 import math
 
 def solution(progresses, speeds):
-    answer = []
-    days = [math.ceil((100 - progress) / speed)  for progress, speed in zip(progresses, speeds)]
-    stack = []
+    days = [math.ceil((100 - progress) / speed) for progress, speed in zip(progresses, speeds)]
     
+    stack = []
+    answer = []
     for day in days:
-        if not stack or day <= stack[0]:
+        if not stack or stack[0] >= day:
             stack.append(day)
         else:
             answer.append(len(stack))
-            stack = [day]
-    print(stack)
+            stack = []
+            stack.append(day)
+            
     answer.append(len(stack))
-    return answer
+    return answer 
+
