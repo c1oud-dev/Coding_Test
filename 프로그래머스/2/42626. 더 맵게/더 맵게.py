ed98475ce8d5a221c5fdb3b18e0a94b66e10f3c2
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-03-13 12:43:18 +0900

    [level 2] Title: 더 맵게, Time: 1638.52 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -5,12 +5,13 @@ def solution(scoville, K):
     heapq.heapify(scoville)
     cnt = 0
     
-    while scoville[0] < K and len(scoville) >= 2:
+    while scoville[0] < K:
+        if len(scoville) < 2:   # 섞어도 K이상이 안 될 때
+            return -1
+        
         first = heapq.heappop(scoville)
         sec = heapq.heappop(scoville)
         heapq.heappush(scoville, first + sec * 2)
         cnt += 1
-        if scoville[0] >= K:
-            return cnt
     
-    return cnt if scoville[0] >= K else -1
+    return cnt
