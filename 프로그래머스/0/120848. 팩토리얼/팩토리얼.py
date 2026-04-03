e5addec0a606a80c13e4d3d881d2d9a0ccb31d64
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-04-03 09:21:34 +0900

    [level 0] Title: 팩토리얼, Time: 0.01 ms, Memory: 9.08 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(n):
+    
+    for i in range(1, 11):
+        if math.factorial(i) > n:
+            return i - 1
+        elif math.factorial(i) == n:
+            return i
