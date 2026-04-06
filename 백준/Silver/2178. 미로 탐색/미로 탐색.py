e2317fc360463ac8bbdefa89662307e0c5382407
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-04-06 18:02:43 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -4,14 +4,13 @@ input = sys.stdin.readline # 입력을 빠르게 받기
 from collections import deque
 
 N, M = map(int, input().split())
-graph = [list(map(str, input().strip())) for _ in range(N)]
+graph = [list(map(int, input().strip())) for _ in range(N)]
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(i, j):
-  queue = deque([(i, j)])
-  graph[i][j] = int(graph[i][j])
+def bfs():
+  queue = deque([(0, 0)])
   
   while queue:
     x, y = queue.popleft()
@@ -23,8 +22,9 @@ def bfs(i, j):
       nx = x + dx[k]
       ny = y + dy[k]
       
-      if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == '1':
-          graph[nx][ny] = int(graph[x][y]) + 1
+      if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 1:
+          graph[nx][ny] = graph[x][y] + 1
           queue.append((nx, ny))
+  return -1
 
-print(bfs(0, 0))
+print(bfs())
