2b8be5711dd949513d52d6d902e31552fa2aef21
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-04-07 09:53:59 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.19 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    alpha = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in my_string:
+        if i.isupper():
+            alpha[ord(i) - 65] += 1
+        else:
+            alpha[ord(i) - 97 + 26] += 1
+    return alpha
