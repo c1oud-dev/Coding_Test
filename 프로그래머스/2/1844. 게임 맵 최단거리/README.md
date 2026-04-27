b94e3101aa0d8af82c6a670b2bd056368f79fe5c
c1oud-dev <117587834+c1oud-dev@users.noreply.github.com>
2026-04-27 09:40:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.52 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 10.68 ms
+메모리: 54.7 MB, 시간: 8.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 31일 10:48:57
+2026년 04월 27일 09:40:57
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] maps) {
+        int n = maps.length, m = maps[0].length;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int x = cur[0], y = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
+                    maps[nx][ny] = maps[x][y] + 1;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int answer = maps[n-1][m-1];
+        return answer > 1 ? answer : -1;
+    }
+}
